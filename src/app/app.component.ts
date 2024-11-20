de7c02170aa5dd396d689fc6b35a45ccdc26e2a9
Shruti Dhoot <shrutidhoot2001@gmail.com>
2024-11-20 19:37:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,11 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Assingn7_2';
+
+   public  BatchName : string   = "Marvellous Infosystems"
+
+  fun(){
+  
+    return this.BatchName
+  }
 }
